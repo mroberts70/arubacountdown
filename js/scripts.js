77bb7e6c30bf42715db7ed6fa587cbcaa18c68ad
Mike Roberts <mroberts@endicott.edu>
2025-05-21 14:08:15 -0400

    blink
    made the countdown blink between blue and red

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,43 @@ function getRandomBackground() {
 
     console.log = currentBackground;
 
-    
-}
-window.onload = function() {
-    getRandomBackground();
 
+}
+
+// Countdown
+// Set the date we're counting down to
+var countDownDate = new Date("Aug 12, 2025 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    var isOdd = new Date().getSeconds() % 2;
+    // console.log(isOdd);
+    
+    if(isOdd === 0) {
+        document.getElementById("the-countdown").style.color = "blue";
+    }
+    else {
+        document.getElementById("the-countdown").style.color = "red";        
+    }
+
+
+    // Output the result in an element with id="the-countdown"
+    document.getElementById("the-countdown").innerHTML = days + "<br>DAYS!";
+
+
+}, 1000);
+
+window.onload = function () {
+    getRandomBackground();
+    document.getElementById("current-year").innerHTML = new Date().getFullYear();
 };
