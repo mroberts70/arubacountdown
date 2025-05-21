5ebed79b318ce077f7f256afba75e9cbf72d62dc
Mike Roberts <mroberts@endicott.edu>
2025-05-21 12:50:48 -0400

    js edit
    js background image link didn't work on the first try. attempting a small edit... removing the step up from the beginning of the link

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 const theBackgrounds = ["bg00001.jpg", "bg00002.jpg", "bg00003.jpg", "bg00004.jpg", "bg00005.jpg", "bg00006.jpg", "bg00007.jpg"];
 
 function getRandomBackground() {
-    let currentBackground = "../images/backgrounds/bg0000";
+    let currentBackground = "images/backgrounds/bg0000";
 
     currentBackground += Math.floor(Math.random() * theBackgrounds.length + 1);
 
