36c30e2577d88f035fcbdc727154d970cec2483e
Mike Roberts <mroberts@endicott.edu>
2025-05-21 12:47:14 -0400

    background randomizer
    added a background image randomizer so I can have the images from all the different aruba vacations as rotating background images

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const theBackgrounds = ["bg00001.jpg", "bg00002.jpg", "bg00003.jpg", "bg00004.jpg", "bg00005.jpg", "bg00006.jpg", "bg00007.jpg"];
+
+function getRandomBackground() {
+    let currentBackground = "../images/backgrounds/bg0000";
+
+    currentBackground += Math.floor(Math.random() * theBackgrounds.length + 1);
+
+    currentBackground += ".jpg";
+
+    document.body.style.backgroundImage = "url(" + currentBackground + ")";
+
+    console.log = currentBackground;
+
+    
+}
+window.onload = function() {
+    getRandomBackground();
+
+};
