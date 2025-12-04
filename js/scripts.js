b575e9a08785100d3ed740733d593445672799b6
Mike Roberts <42217803+mroberts70@users.noreply.github.com>
2025-12-04 13:54:12 -0500

    fixed the js file

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function getRandomBackground() {
 
 // Countdown
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 12, 2025 23:59:59").getTime();
+var countDownDate = new Date("Mar 18, 2026 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
